624d5a4834411c863854e6d64474e6a5ffa74d3b
Kvasir-reborn <123034787+Kvasir-reborn@users.noreply.github.com>
2023-04-20 12:44:26 -0500

    Finish up all functionality.

--- spreadsheetToTextFile.py
+++ spreadsheetToTextFile.py
@@ -4,18 +4,57 @@
 
 import logging
 import openpyxl
+from openpyxl.utils import get_column_letter
 
 logging.basicConfig(
     level=logging.DEBUG,
     filename="logging.txt",
     format="%(asctime)s -  %(levelname)s -  %(message)s",
 )
+# logging.disable(logging.CRITICAL)
 
 
 def open_workbook():
-    workbook = openpyxl.load_workbook("text_to_spread.xlsx")
+    """Open user-designated Excel file in current directory and return its sheet contents."""
+    file_name = f'{input("Please enter file name here: ")}.xlsx'
+    workbook = openpyxl.load_workbook(filename=file_name)
     sheet = workbook.active
-    logging.debug(sheet)
+    return sheet
 
 
-open_workbook()
+def record_contents(sheet):
+    """Record contents of rows per column of sheet into a list of lists and return list."""
+    file_contents = []
+
+    for column in range(sheet.max_column):
+        sheet_contents = []
+        column_letter = get_column_letter(column + 1)
+
+        for row in range(1, sheet.max_row + 1):
+            cell_value = sheet[f"{column_letter}{row}"].value
+
+            if cell_value is not None:
+                sheet_contents.append(cell_value)
+
+        file_contents.append(sheet_contents)
+    return file_contents
+
+
+def write_contents(contents):
+    """Write contents of each line of secondary list to file associated with primary list value."""
+    for index_1, _ in enumerate(contents):
+        with open(
+            f"Text_file_{str(index_1 + 1).zfill(3)}.txt", "w", encoding="utf-8"
+        ) as txt:
+            for index_2, _ in enumerate(contents[index_1]):
+                txt.write(contents[index_1][index_2])
+
+
+def main_func():
+    """Runs prior functions in sequence."""
+    user_sheet = open_workbook()
+    file_contents = record_contents(user_sheet)
+    write_contents(file_contents)
+
+
+main_func()
