0a9fb66cebc76f8b3d88f2aa989e600d953c2225
Kvasir-reborn <123034787+Kvasir-reborn@users.noreply.github.com>
2023-08-27 19:52:03 -0500

    Add requirements.txt and tidy up write_contents function.

--- spreadsheet_to_text_file.py
+++ spreadsheet_to_text_file.py
@@ -3,7 +3,7 @@
 # For more information, see README.md
 
 import logging
-import openpyxl
+from openpyxl import load_workbook
 from openpyxl.utils import get_column_letter
 
 logging.basicConfig(
@@ -11,21 +11,19 @@ logging.basicConfig(
     filename="logging.txt",
     format="%(asctime)s -  %(levelname)s -  %(message)s",
 )
-# logging.disable(logging.CRITICAL)
-
-file_name = f'{input("Please enter file name here: ")}'
-plus_extension = f"{file_name}.xlsx"
+logging.disable(logging.CRITICAL)  # Note out to enable logging.
 
 
-def open_workbook(file):
-    """Open user-designated Excel file in current directory and return its sheet contents."""
-    workbook = openpyxl.load_workbook(filename=file)
-    sheet = workbook.active
-    return sheet
+def main() -> None:
+    """Runs functions in sequence on user-defined input."""
+    file_name = input("Please enter file name here: ")
+    file_contents = record_contents(file_name)
+    write_contents(file_contents, file_name)
 
 
-def record_contents(sheet):
-    """Record contents of rows per column of sheet into a list of lists and return list."""
+def record_contents(file: str) -> list[list[str]]:
+    """Open user-designated Excel file in current directory and record contents of rows per column of sheet into a list of lists, then return list."""
+    sheet = load_workbook(f"{file}.xlsx").active
     return [
         [
             sheet[f"{get_column_letter(column + 1)}{row}"].value
@@ -36,21 +34,12 @@ def record_contents(sheet):
     ]
 
 
-def write_contents(contents):
+def write_contents(contents: str, file_name: str) -> None:
     """Write contents of each line of secondary list to file associated with primary list value."""
-    for index_1, _ in enumerate(contents):
-        with open(
-            f"{file_name}_column{str(index_1 + 1).zfill(3)}.txt", "w", encoding="utf-8"
-        ) as txt:
-            for index_2, _ in enumerate(contents[index_1]):
-                txt.write(str(contents[index_1][index_2]))
-
-
-def main():
-    """Runs prior functions in sequence."""
-    user_sheet = open_workbook(plus_extension)
-    file_contents = record_contents(user_sheet)
-    write_contents(file_contents)
+    for i, _ in enumerate(contents):
+        with open(f"{file_name}_column{(i + 1):03}.txt", "w", encoding="utf-8") as txt:
+            for j, _ in enumerate(contents[i]):
+                txt.write(str(contents[i][j]))
 
 
 if __name__ == "__main__":
